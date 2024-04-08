45ac882131774c39dbdc449b497c70322f520bac
weterynarzfred <weterynarzfred@gmail.com>
2024-04-08 10:25:53 +0200

    switch to swc

--- webpack.config.js
+++ webpack.config.js
@@ -49,11 +49,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime'],
-          },
+          loader: 'swc-loader',
         },
       },
       {
